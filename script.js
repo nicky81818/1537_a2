038a2b80e32093df8eb87e888d76dc87d44b78bc
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-03-05 21:07:18 -0800

    added functionality to next and previous buttons

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ $(document).ready(
         $.ajax({
             url: "https://yts.mx/api/v2/list_movies.json?sort_by=rating",
             success: function (res) {
-                twenty_movies = res.data.movies.slice(0, 2)
+                two_movies = res.data.movies.slice(0, 2)
 
-                twenty_movies.forEach(function (movie) {
+                two_movies.forEach(function (movie) {
                     $("main").append(
                         `
                         <div class="movie">
@@ -36,9 +36,9 @@ $(document).ready(
                 url: "https://yts.mx/api/v2/list_movies.json?sort_by=rating",
                 success: function (res) {
                     id_num = id_num * 2
-                    twenty_movies = res.data.movies.slice(id_num - 2, id_num)
+                    two_movies = res.data.movies.slice(id_num - 2, id_num)
 
-                    twenty_movies.forEach(function (movie) {
+                    two_movies.forEach(function (movie) {
                         $("main").append(
                             `
                             <div class="movie">
@@ -49,6 +49,112 @@ $(document).ready(
                     })
                 }
             })
+        }
+    )
+    $(".next").click(
+        function () {
+            $(".prev").removeClass("greyed")
+            previous_button = $(".active").attr("id")
+            if (previous_button < 9) {
+                previous_button = "#" + previous_button
+                $(".active").next().addClass("active")
+                $(previous_button).removeClass("active")
+                $("main").children().remove()
+                $.ajax({
+                    url: "https://yts.mx/api/v2/list_movies.json?sort_by=rating",
+                    success: function (res) {
+                        previous_button = parseInt(previous_button.slice(1)) * 2
+                        two_movies = res.data.movies.slice(previous_button, previous_button + 2)
+
+                        two_movies.forEach(function (movie) {
+                            $("main").append(
+                                `
+                                <div class="movie">
+                                    <img src="${movie.medium_cover_image}" alt="${movie.title}">
+                                </div>
+                                `
+                            )
+                        })
+                    }
+                })
+            }
+            if (previous_button == 9) {
+                previous_button = "#" + previous_button
+                $(".active").next().addClass("active")
+                $(previous_button).removeClass("active")
+                $(this).addClass("greyed")
+                $("main").children().remove()
+                    $.ajax({
+                        url: "https://yts.mx/api/v2/list_movies.json?sort_by=rating",
+                        success: function (res) {
+                            previous_button = parseInt(previous_button.slice(1)) * 2
+                            two_movies = res.data.movies.slice(previous_button, previous_button + 2)
+
+                            two_movies.forEach(function (movie) {
+                                $("main").append(
+                                    `
+                                    <div class="movie">
+                                        <img src="${movie.medium_cover_image}" alt="${movie.title}">
+                                    </div>
+                                    `
+                                )
+                            })
+                        }
+                    })
+                }
+            }
+            )
+            $(".prev").click(
+                function () {
+                    $(".next").removeClass("greyed")
+                    next_button = $(".active").attr("id")
+                    if (next_button > 2) {
+                        next_button = "#" + next_button
+                        $(".active").prev().addClass("active")
+                        $(next_button).removeClass("active")
+                        $("main").children().remove()
+                            $.ajax({
+                                url: "https://yts.mx/api/v2/list_movies.json?sort_by=rating",
+                                success: function (res) {
+                                    next_button = (parseInt(next_button.slice(1)) * 2) - 2
+                                    two_movies = res.data.movies.slice(next_button - 2, next_button)
+                
+                                    two_movies.forEach(function (movie) {
+                                        $("main").append(
+                                            `
+                                            <div class="movie">
+                                                <img src="${movie.medium_cover_image}" alt="${movie.title}">
+                                            </div>
+                                            `
+                                        )
+                                    })
+                                }
+                            })
+                        }
+                        if (next_button == 2) {
+                            next_button = "#" + next_button
+                            $(".active").prev().addClass("active")
+                            $(next_button).removeClass("active")
+                            $(this).addClass("greyed")
+                            $("main").children().remove()
+                                $.ajax({
+                                    url: "https://yts.mx/api/v2/list_movies.json?sort_by=rating",
+                                    success: function (res) {
+                                        next_button = (parseInt(next_button.slice(1)) * 2) - 2
+                                        two_movies = res.data.movies.slice(next_button - 2, next_button)
+                    
+                                        two_movies.forEach(function (movie) {
+                                            $("main").append(
+                                                `
+                                                <div class="movie">
+                                                    <img src="${movie.medium_cover_image}" alt="${movie.title}">
+                                                </div>
+                                                `
+                                            )
+                                        })
+                                    }
+                                })
+            }
         }
     )
 })
